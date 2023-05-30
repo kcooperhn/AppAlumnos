01d8bb3e7d3c67e7c03c9002833e04e62f794aa6
kcooperhn <134466354+kcooperhn@users.noreply.github.com>
2023-05-29 21:02:11 -0600

    Manejando click de botón
    Manejando el evento click del botón

--- app/src/main/java/hn/uth/appalumnos/MainActivity.java
+++ app/src/main/java/hn/uth/appalumnos/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import hn.uth.appalumnos.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        binding.btnSaludar.setOnClickListener(v -> {
+            binding.txtTitulo.setText("Bienvenido "+binding.edtNombre.getText());
+        });
+
     }
 }
